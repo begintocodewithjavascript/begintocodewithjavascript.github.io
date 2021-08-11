ddae73baade792b89922985aa70749671de1cc9b
Rob Miles <rob@robmiles.com>
2021-08-11 13:54:25 +0100

    Release updates

--- code/Ch11 Creating Applications/Ch11-15 Addition Server/additionServer.js
+++ code/Ch11 Creating Applications/Ch11-15 Addition Server/additionServer.js
@@ -1,19 +1,19 @@
-var http = require('http'); 
-var url = require('url'); 
+var http = require('http');
+var url = require('url');
 
-var server = http.createServer((request, response) => { 
+var server = http.createServer((request, response) => {
 
-var parsedUrl = url.parse(request.url,true);
-var queries = parsedUrl.query;
+  var parsedUrl = url.parse(request.url, true);
+  var queries = parsedUrl.query;
 
-  
-var no1 = Number(queries.no1); 
-var no2 = Number(queries.no2); 
 
-var result= no1 + no2; 
+  var no1 = Number(queries.no1);
+  var no2 = Number(queries.no2);
+
+  var result = no1 + no2;
 
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write(String(result)); 
+  response.write(String(result));
   response.end();
 
 });
